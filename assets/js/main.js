93bd17aae99d73c5744dc913008066ea36046002
Marcelo Eliseu <maeliseu@gmail.com>
2021-11-17 15:43:37 -0300

    page proj 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,4 +122,22 @@
     })
   });
 
+
+  /**
+   * Portfolio details slider
+   */
+   new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
 })()
